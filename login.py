2192a80c2574f44b660c415e767a0f8f9e0a84ff
stayesix <belozerova06@internet.ru>
2025-10-20 12:17:07 +0700

    Добавлено безопасное хеширование паролей

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-def check_password(password):
-    if len(password) >= 8: 
-        return True
-    return False
+import hashlib
+def check_password(input_password, stored_hash):
+    input_hash = hashlib.sha256(input_password.encode()).hexdigest()
+    return input_hash == stored_hash
