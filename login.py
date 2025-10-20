bc75749490005fb2cb34a2dd2be873a0d33b38cc
stayesix <belozerova06@internet.ru>
2025-10-20 12:18:19 +0700

    Добавлена документация и финальные улучшения

--- login.py
+++ login.py
@@ -1,3 +1,8 @@
+"""
+Модуль для безопасной аутентификации пользователей
+Версия: 1.0
+Исправлены уязвимости безопасности
+"""
 import hashlib
 def check_password(input_password, stored_hash):
     input_hash = hashlib.sha256(input_password.encode()).hexdigest()
